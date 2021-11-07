841c4e0d1f6f881eca90857bb97de7b29346178b
AleGL92 <garcialagos.alejandro@gmail.com>
2021-11-07 11:06:07 +0100

    leyendo

--- pruebi.py
+++ pruebi.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-""" ruta_local = 'E:\ALEJANDRO\DATA_SCIENCE\POWER_BI\POWER_BI_NACHO\PANEL_PRACTICA\PRUEBAS_GIT\pruebi_exel.csv'
-ruta_git = ''
-pruebi = pd.read_csv() """
+# ruta_local = 'E:\ALEJANDRO\DATA_SCIENCE\POWER_BI\POWER_BI_NACHO\PANEL_PRACTICA\PRUEBAS_GIT\pruebi_exel.csv'
+ruta_git = 'https://raw.githubusercontent.com/AleGL92/PowerBI/master/pruebi_exel.csv'
+pruebi = pd.read_csv(ruta_git)
+pruebi.head()
