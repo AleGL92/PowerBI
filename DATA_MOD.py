cb72deb1d166fd63b68dbb7aecf5f3ae4448cf14
AleGL92 <garcialagos.alejandro@gmail.com>
2021-11-07 17:54:06 +0100

    Preparing data

--- DATA_MOD.py
+++ DATA_MOD.py
@@ -1,7 +1,16 @@
 # We want to modify the data in the column 'CANTIDAD' and delete the column 'MATRICULA' in both CSV files
 import pandas as pd
-url_18 = 'https://raw.githubusercontent.com/AleGL92/PowerBI/master/Entradas_18_19.csv'
-d18_19 = pd.read_csv(url_18, encoding = 'latin-1')
-print(d18_19.head())
 
-# tiene errores
+url_18_19 = 'E:\ALEJANDRO\DATA_SCIENCE\POWER_BI\POWER_BI_NACHO\PANEL_PRACTICA\PRUEBAS_GIT\Entradas_18_19.csv'
+url_20 = 'E:\ALEJANDRO\DATA_SCIENCE\POWER_BI\POWER_BI_NACHO\PANEL_PRACTICA\PRUEBAS_GIT\Entradas_20.csv'
+d18_19 = pd.read_csv(url_18_19, header = 0, sep = ';')
+d20 = pd.read_csv(url_20, header = 0, sep = ';')
+
+# print(d18_19.head(5))
+# print(d20.head(5))
+
+url_18_19['cantidad'] = url_18_19['cantidad']*1000
+url_20['cantidad'] = url_20['cantidad']*1000
+
+url_18_19 =  url_18_19.drop('matricula', 1)
+url_20 = url_20.drop('matricula', 1)
