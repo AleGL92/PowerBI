4c926120873b5b2addf277b10af5b2c8cc1c72a3
AleGL92 <garcialagos.alejandro@gmail.com>
2021-11-07 11:55:13 +0100

    1er mod

--- pruebi.py
+++ pruebi.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 ruta_local = 'E:\ALEJANDRO\DATA_SCIENCE\POWER_BI\POWER_BI_NACHO\PANEL_PRACTICA\PRUEBAS_GIT\pruebi_exel.csv'
 ruta_git = 'https://raw.githubusercontent.com/AleGL92/PowerBI/master/pruebi_exel.csv'
-pruebi = pd.read_csv(ruta_git, header = 0, sep = ',')
+pruebi = pd.read_csv(ruta_local, header = 0, sep = ',')
 print(pruebi.head())
 
-# pruebi['salario'] = pruebi['salario']/1000
+# pruebi['salario'] = pruebi['salario']*1000
 # print(pruebi)
 
 # pruebi.to_csv(ruta_local, index = False)
